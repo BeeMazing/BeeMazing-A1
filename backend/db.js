658ec50f2ad2ca5f8d3e83563b64403661838ce7
g4meChanger <g4mechanger888@gmail.com>
2025-04-08 19:59:05 +0300

    BeeMazing 2.0 v5

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,7 @@
 const { MongoClient } = require('mongodb');
 
 const uri = "mongodb+srv://g4mechanger888:wearewe12@beemazing.mniyzbt.mongodb.net/?retryWrites=true&w=majority&appName=BeeMazing";
+
 const client = new MongoClient(uri);
 
 async function connectDB() {
