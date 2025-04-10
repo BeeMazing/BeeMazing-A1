f1bcb9b501e883056e6d7ab0d10a719b6ce34209
g4meChanger <g4mechanger888@gmail.com>
2025-04-10 20:00:02 +0300

    v2

--- web/1-Home/home.js
+++ web/1-Home/home.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const urlParams = new URLSearchParams(window.location.search);
-const adminFromURL = urlParams.get("admin");
-if (adminFromURL) {
-    localStorage.setItem("currentAdminEmail", adminFromURL);
-}
+    const adminFromURL = urlParams.get("admin");
+    localStorage.setItem("currentAdminEmail", "null"); // Hardcode for now
+    console.log("home.js set adminEmail to: null");
 
 
     // Redirect to login if user not logged in
